0f450b66a23f23e64130802716b8c7fe1179c1fc
flow <54749904+flow1777@users.noreply.github.com>
2023-07-09 23:05:34 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+
+namespace MemoryScanner
+{
+    public class MemoryCleaner
+    {
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int PROCESS_WM_READ = 0x0010;
+
+        const int PAGE_READWRITE = 0x04;
+        const int MEM_COMMIT = 0x1000;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(int hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(int hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll")]
+        public static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, int dwLength);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MEMORY_BASIC_INFORMATION
+        {
+            public IntPtr BaseAddress;
+            public IntPtr AllocationBase;
+            public int AllocationProtect;
+            public IntPtr RegionSize;
+            public int State;
+            public int Protect;
+            public int Type;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            ushort reserved;
+            public uint pageSize;
+            public IntPtr minimumApplicationAddress;
+            public IntPtr maximumApplicationAddress;
+            public IntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
+
+        private static Process GetProcessByName(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length > 0)
+            {
+                return processes[0];
+            }
+
+            Process process = null;
+
+            try
+            {
+                var query = $"SELECT ProcessId FROM Win32_Service WHERE Name='{processName}'";
+                var searcher = new ManagementObjectSearcher("root\\CIMV2", query);
+                var results = searcher.Get();
+
+                foreach (var result in results)
+                {
+                    var processId = (uint)result["ProcessId"];
+                    process = Process.GetProcessById((int)processId);
+                    break;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return process;
+        }
+
+        static ServiceController GetServiceByName(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+
+            foreach (ServiceController service in services)
+            {
+                if (service.ServiceName.StartsWith(serviceName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return service;
+                }
+            }
+
+            return null;
+        }
+
+        static int GetProcessIdFromService(ServiceController service)
+        {
+            using (ManagementObject serviceObj = new ManagementObject($"Win32_Service.Name='{service.ServiceName}'"))
+            {
+                object processIdObj = serviceObj["ProcessId"];
+                if (processIdObj != null && int.TryParse(processIdObj.ToString(), out int processId))
+                {
+                    return processId;
+                }
+            }
+
+            return 0;
+        }
+
+        public class CliArgs
+        {
+            public string searchterm { get; set; }
+            public int prepostfix { get; set; }
+            public int delay { get; set; }
+            public string mode { get; set; }
+        }
+
+        public static void Main()
+        {
+            Dictionary<string, List<string>> processToSearchStrings = new Dictionary<string, List<string>>
+            {
+                { "lsass", new List<string> {
+                  "skript",
+                  "keyauth",
+            } },
+                { "dnscache", new List<string> { "skript", "keyauth" } }
+            };
+
+            foreach (var kvp in processToSearchStrings)
+            {
+                string processName = kvp.Key;
+                List<string> searchStrings = kvp.Value;
+
+                Process process = GetProcessByName(processName);
+                if (process != null)
+                {
+                    foreach (string searchString in searchStrings)
+                    {
+                        CliArgs myargs = new CliArgs
+                        {
+                            searchterm = searchString,
+                            prepostfix = 10,
+                            delay = 1000,
+                            mode = "stdio"
+                        };
+
+                        var targetStrings = memScanString(process, myargs);
+
+                        if (targetStrings.Count > 0)
+                        {
+                            ReplaceStringInProcessMemory(process, targetStrings);
+                        }
+                    }
+                }
+                else
+                {
+                    ServiceController service = GetServiceByName(processName);
+                    if (service != null)
+                    {
+                        int processId = GetProcessIdFromService(service);
+
+                        if (processId > 0)
+                        {
+                            Process associatedProcess = Process.GetProcessById(processId);
+
+                            foreach (string searchString in searchStrings)
+                            {
+                                CliArgs myargs = new CliArgs
+                                {
+                                    searchterm = searchString,
+                                    prepostfix = 10,
+                                    delay = 1000,
+                                    mode = "stdio"
+                                };
+
+                                var targetStrings = memScanString(associatedProcess, myargs);
+
+                                if (targetStrings.Count > 0)
+                                {
+                                    ReplaceStringInProcessMemory(associatedProcess, targetStrings);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static Dictionary<long, string> memScanString(Process process, CliArgs myargs)
+        {
+            IntPtr processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_WM_READ, false, process.Id);
+
+            SYSTEM_INFO sys_info = new SYSTEM_INFO();
+            GetSystemInfo(out sys_info);
+
+            IntPtr proc_min_address = sys_info.minimumApplicationAddress;
+            IntPtr proc_max_address = sys_info.maximumApplicationAddress;
+
+            var targetStrings = new Dictionary<long, string>();
+
+            while (proc_min_address.ToInt64() < proc_max_address.ToInt64())
+            {
+                VirtualQueryEx(processHandle, proc_min_address, out MEMORY_BASIC_INFORMATION mem_basic_info, Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+
+                if (mem_basic_info.Protect == PAGE_READWRITE && mem_basic_info.State == MEM_COMMIT)
+                {
+                    byte[] buffer = new byte[(int)mem_basic_info.RegionSize];
+
+                    ReadProcessMemory(processHandle.ToInt32(), mem_basic_info.BaseAddress, buffer, (int)mem_basic_info.RegionSize, out int bytesRead);
+
+                    string memStringASCII = Encoding.ASCII.GetString(buffer);
+                    string memStringUNICODE = Encoding.Unicode.GetString(buffer);
+
+                    if (memStringASCII.Contains(myargs.searchterm))
+                    {
+                        int index = 0;
+                        while ((index = memStringASCII.IndexOf(myargs.searchterm, index)) != -1)
+                        {
+                            IntPtr address = (IntPtr)((long)mem_basic_info.BaseAddress + index);
+                            int length = myargs.searchterm.Length;
+
+                            long addressKey = address.ToInt64();
+                            if (!targetStrings.ContainsKey(addressKey))
+                            {
+                                targetStrings.Add(addressKey, memStringASCII.Substring(index - myargs.prepostfix, length + myargs.prepostfix * 2));
+                            }
+
+                            index++;
+                        }
+                    }
+
+                    if (memStringUNICODE.Contains(myargs.searchterm))
+                    {
+                        int index = 0;
+                        while ((index = memStringUNICODE.IndexOf(myargs.searchterm, index)) != -1)
+                        {
+                            IntPtr address = (IntPtr)((long)mem_basic_info.BaseAddress + index * 2);
+                            int length = myargs.searchterm.Length;
+
+                            long addressKey = address.ToInt64();
+                            if (!targetStrings.ContainsKey(addressKey))
+                            {
+                                targetStrings.Add(addressKey, memStringUNICODE.Substring(index - myargs.prepostfix, length + myargs.prepostfix * 2));
+                            }
+
+                            index++;
+                        }
+                    }
+                }
+
+                long size = mem_basic_info.RegionSize.ToInt64();
+                if (size > int.MaxValue)
+                {
+                    size = int.MaxValue;
+                }
+                proc_min_address = IntPtr.Add(mem_basic_info.BaseAddress, (int)size);
+            }
+
+            CloseHandle(processHandle);
+
+            return targetStrings;
+        }
+
+        public static void ReplaceStringInProcessMemory(Process process, Dictionary<long, string> targetStrings)
+        {
+            foreach (KeyValuePair<long, string> stringInMemory in targetStrings)
+            {
+                long address = stringInMemory.Key;
+                string str = stringInMemory.Value;
+                byte[] bytes = Encoding.ASCII.GetBytes(str);
+                byte[] replacementBytes = new byte[bytes.Length];
+                int num;
+                MemoryCleaner.WriteProcessMemory(process.Handle.ToInt32(), (IntPtr)address, replacementBytes, (uint)replacementBytes.Length, out num);
+            }
+        }
+    }
+}
